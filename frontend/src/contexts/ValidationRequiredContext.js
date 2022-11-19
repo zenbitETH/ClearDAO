2f228554334b2ab48fdc667d631e47d75c343fd6
HabacucMX <habacuc.vera@gmail.com>
2022-11-19 10:33:52 -0600

    Hardhat working on FEVM

--- frontend/src/contexts/ValidationRequiredContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from 'react';
-
-export const ValidationRequiredContext = createContext({});
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,94 +1,12 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require('dotenv').config()
-const fs = require("fs");
+require("@nomicfoundation/hardhat-toolbox");
+require('hardhat-deploy');
+require('hardhat-deploy-ethers');
+require("./tasks")
+require("dotenv").config()
 
-const defaultNetwork = "wallaby";
-
-function mnemonic() {
-  try {
-    return fs.readFileSync("./mnemonic.txt").toString().trim();
-  } catch (e) {
-    if (defaultNetwork !== "wallaby") {
-    }
-  }
-  return "";
-}
-
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork,
-
-  networks: {
-    hardhat: {
-      chainId: 1337
-    },
-    localhost: {
-      url: "http://localhost:8545",
-    },
-    wallaby: {
-      hardhat: {
-        chainId: 4554
-      },
-      url: "https://wallaby.node.glif.io/rpc/v0",
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
-      //accounts: {
-      //  mnemonic: mnemonic(),
-      //},
-    },
-    //skale: {
-    //  url: "https://eth-global-10.skalenodes.com:10200",
-    //  accounts: [`0x${process.env.PRIVATE_KEY}`]
-    //},
-    rinkeby: {
-      url: "https://rinkeby.infura.io/v3/faefe1dcd6094fb388019173d2328d8f",
-      accounts: {
-        mnemonic: mnemonic(),
-      },
-    },
-    mainnet: {
-      url: "https://mainnet.infura.io/v3/faefe1dcd6094fb388019173d2328d8f",
-      accounts: {
-        mnemonic: mnemonic(),
-      },
-    },
-    //ropsten: {
-    //  url: "https://ropsten.infura.io/v3/b98747734bab473a99d0fe366eb065a0",
-    //  accounts: [`0x${process.env.PRIVATE_KEY}`]
-    //  //accounts: {
-    //  //  mnemonic: mnemonic(),
-    //  //},
-    //},
-
-    mumbai: {
-      url: "https://polygon-mumbai.infura.io/v3/6842d8c76d0943b2b5f9a4a269b62484",
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
-      //accounts: {
-      //  mnemonic: mnemonic(),
-      //},
-    },
-    
-      //polygon: {
-      //  url: "https://ropsten.infura.io/v3/b98747734bab473a99d0fe366eb065a0",
-      //  accounts: [`0x${process.env.PRIVATE_KEY}`]
-      //accounts: {
-      //  mnemonic: mnemonic(),
-      //},
-      //},
-
-    kovan: {
-      url: "https://kovan.infura.io/v3/faefe1dcd6094fb388019173d2328d8f",
-      accounts: {
-        mnemonic: mnemonic(),
-      },
-    },
-    xdai: {
-      url: 'https://dai.poa.network',
-      gasPrice: 1000000000,
-      accounts: {
-        mnemonic: mnemonic(),
-      },
-    },
-  },
   solidity: {
     compilers: [
       {
@@ -111,29 +29,17 @@ module.exports = {
       runs: 200,
     },
   },
-
-  etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY
-      // add other network's API key here
-  },
-
-  abiExporter: {
-    runOnCompile: true,
-    clear: true,
-    flat: true,
-    only: [],
-    spacing: 2,
-    pretty: false,
+  defaultNetwork: "wallaby",
+  networks: {
+    wallaby: {
+      url: "https://wallaby.node.glif.io/rpc/v0",
+      accounts: [PRIVATE_KEY],
+    }
   },
   paths: {
     sources: "./contracts",
     tests: "./test",
     cache: "./cache",
-    artifacts: './frontend/src/contracts'
-
+    artifacts: "./artifacts"
   },
-  mocha: {
-    timeout: 10000000
-  }
-
 };
