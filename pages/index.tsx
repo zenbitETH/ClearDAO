d075cfa013f3c9ea59e42c8b1f60589ccd8f0f35
HabacucMX <habacuc.vera@gmail.com>
2022-11-19 15:27:39 -0600

    abi.json added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import ethers from "ethers";
-import abi from '../../abi/abi.json'
+import abi from '../abi/abi.json'
 
 async function vote (){
   const ethereum = (window as any).ethereum;
