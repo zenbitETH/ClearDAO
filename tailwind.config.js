f6886edc4f1bbaff93fa4780685c8777595b88fc
HabacucMX <habacuc.vera@gmail.com>
2022-11-20 08:47:09 -0600

    colors and background update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+				display: ["group-hover"],
+				  clear: {
+				    900: "#8A58CA",
+				    800: "#0077E4",
+				    600: "#008AD9",
+				    700: "#0093B2",
+				    500: "#C52993",//Main
+				    400: "#6473e0",
+				    300: "#95a0ea",
+				    200: "#b6bdf1",
+				    100: "#00977F",
+			  },
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
