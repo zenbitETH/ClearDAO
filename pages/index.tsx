b1d11339adcae5a7ee08335b011df56e8643bdf6
HabacucMX <habacuc.vera@gmail.com>
2022-11-20 08:25:08 -0600

    typo in abi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,5 @@
 import type { NextPage } from 'next';
-import ethers from "ethers";
-import abi from '../abi/abi.json';
 import Dashboard from '../pages/dashboard'
-
-async function vote (){
-  const ethereum = (window as any).ethereum;
-    const accounts = await ethereum.request({
-      method: "eth_requestAccounts",
-    });
-
-    const provider = new ethers.providers.Web3Provider(ethereum)
-    const walletAddress = accounts[0]    // first account in MetaMask
-    const signer = provider.getSigner(walletAddress)
-    const GovernorAlpha = new ethers.Contract("0xdAC17F958D2ee523a2206206994597C13D831ec7", abi, signer) 
-    const vote = await GovernorAlpha.castVote(1,true)
-}
-
 
 const Home: NextPage = () => {
   return (
